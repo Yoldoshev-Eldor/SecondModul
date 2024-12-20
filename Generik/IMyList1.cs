8d8c4a9337f68e991f775444257a2db244a3a483
ibragimovdev7 <89067487+ibragimovdev7@users.noreply.github.com>
2024-12-20 19:00:39 +0500

    Generik

---
+++ Generik/IMyList1.cs
@@ -0,0 +1,13 @@
+﻿namespace Generik
+{
+    public interface IMyList<T>
+    {
+        public void AddItem(T item);
+        public T GetItemAt(int index);
+        public void RemoveItemAt(int index);
+        public void ReplaceAllItems(T oldElement, T newElement);
+        public bool Contains(T item);
+        public void Clear();
+        public void InsertAt(int index, T item);
+    }
+}
---
+++ Generik/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
