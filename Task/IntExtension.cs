59071b192b089cc2f10423567f5707fed3fd31c7
ibragimovdev7 <89067487+ibragimovdev7@users.noreply.github.com>
2024-12-22 18:24:16 +0500

    Lesson8 HMWR uploaded

---
+++ Task/IntExtension.cs
@@ -0,0 +1,36 @@
+﻿namespace Task
+{
+    public static class IntExtension
+    {
+        public static bool CheckEven(this int value)
+        {
+            if (value % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool CheckToPrimeNumber(this int value)
+        {
+            var count = 0;
+            for (int i = 1; i <= value; ++i)
+            {
+                if (value % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static void AddNumber(this int value, int number)
+        {
+            value += number;
+        }
+    }
+    
+    
+}
---
+++ Task/StringBuilderExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Task;
+
+public static class StringBuilderExtension
+{
+    public static int CounterIsUpper(this StringBuilder text)
+    {
+        var count = 0;
+        for (var i = 0; i < text.Length; i++)
+        {
+            if (char.IsUpper(text[i]))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
